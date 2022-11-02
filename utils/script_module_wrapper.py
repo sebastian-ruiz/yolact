b6b247dbdd467a8d436546fb15acf3549015ea94
Sebastian Ruiz <sebastian.ruiz@uni-goettingen.de>
2022-11-02 11:37:10 +0100

    bug fix

--- utils/script_module_wrapper.py
+++ utils/script_module_wrapper.py
@@ -6,5 +6,5 @@ use_jit = torch.cuda.device_count() <= 1
 if not use_jit:
     print('Multiple GPUs detected! Turning off JIT.')
 
-ScriptModuleWrapper = torch.jit.ScriptModule if use_jit else nn.Module #TODO remove once nn.Module supports JIT script modules
+ScriptModuleWrapper = torch.jit.ScriptModule if use_jit else torch.nn.Module #TODO remove once nn.Module supports JIT script modules
 script_method_wrapper = torch.jit.script_method if use_jit else lambda fn, _rcn=None: fn
